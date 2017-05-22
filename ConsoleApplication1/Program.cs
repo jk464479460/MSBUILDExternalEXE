a596f42fbb4be5633058780074a74a98552fb6b9
Xiaoyan Wu <xiaoyan.wu@cortlandglobal.com>
2017-05-22 13:14:53 +0800

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var w=new StreamWriter("D:\\log.txt", true, Encoding.UTF8))
+            {
+                w.WriteLine(DateTime.Now);
+            }
+            Console.WriteLine("Write Successfully");
+            Console.Read();
+        }
+    }
+}
